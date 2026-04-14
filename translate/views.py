a24ac40e7605bc7ed6d9b8c3962b891b754eb6b9
topango <opangotimothy14@gmail.com>
2026-04-14 13:28:05 +0300

    Added input validation and error handling

--- translate/views.py
+++ translate/views.py
@@ -16,11 +16,19 @@ def translate_text_api(request):
         data = json.loads(request.body.decode('utf-8'))
         text = data.get("text", "")
         target_language = data.get("targetLanguage", "")
+
+        if not text:
+            return JsonResponse({"error": "Text field is required"}, status=400)
+        if target_language not in ["en", "sw"]:
+            return JsonResponse({"error": "Language not found"}, status=400)
+
         translated_text = translate_text(text, target_language)
 
         return JsonResponse({
             "translatedText": translated_text
         })
     
+    except json.JSONDecodeError:
+        return JsonResponse({"error": "Invalid Format"}, status=400)
     except Exeption as e:
         return JsonResponse({"error": str(e)}, status=500)
