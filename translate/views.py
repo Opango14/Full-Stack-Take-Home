6141efe5dc0773ca895d31cecee644463351c8ca
topango <opangotimothy14@gmail.com>
2026-04-14 13:19:16 +0300

    Added basic API endpoint and index.html

--- translate/views.py
+++ translate/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+import json
 
-# Create your views here.
+from .translation import translate_text
+
+def index(request):
+    return render(request, 'translate/index.html')
+
+@csrf_exempt
+@require_http_methods(["POST"])
+def translate_text_api(request):
+    try:
+        data = json.loads(request.body.decode('utf-8'))
+        text = data.get("text", "")
+        target_language = data.get("targetLanguage", "")
+        translated_text = translate_text(text, target_language)
+
+        return JsonResponse({
+            "translatedText": translated_text
+        })
+    
+    except Exeption as e:
+        return JsonResponse({"error": str(e)}, status=500)
